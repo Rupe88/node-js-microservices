543adf021bdc27e0a08a5a66e8c664ca5f23252b
Rupee88 <chyrupesh828@gmail.com>
2025-01-12 23:48:12 +0545

    Connected Identity-Service With Api-Gateway Service

---
+++ api-gateway/src/server.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const Redis = require('ioredis');
+const helmet = require('helmet');
+const { rateLimit } = require('express-rate-limit');
+const { RedisStore } = require('rate-limit-redis');
+const logger = require('./utils/logger');
+const proxy = require('express-http-proxy');
+const errorHandler = require('./middleware/errorHandler');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const redisClient = new Redis(process.env.REDIS_URL);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+//rate limitting
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+  handler: (req, res, next) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP:${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: 'Too many requests',
+    });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+app.use(rateLimiter);
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body ${req.body} request to ${req.body}`);
+  next();
+});
+
+const proxyOptions = {
+  proxyReqPathResolver: (req) => {
+    return req.originalUrl.replace(/^\/v1/, '/api');
+  },
+  proxyErrorHandler: (err, req, res, next) => {
+    logger.err(`Proxy error: ${err.message}`);
+    res.status(500).json({
+      message: 'Internal server error',
+      error: err.message,
+    });
+  },
+};
+
+//setting up proxy for our identity service
+app.use(
+  '/v1/auth',
+  proxy(process.env.IDENTITY_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers['Content-Type'] = 'application/json';
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response Received from Identity Service:${proxyRes.statusCode}`
+      );
+
+      return proxyResData;
+    },
+  })
+);
+
+app.use(errorHandler);
+app.listen(PORT, () => {
+  logger.info(`Api gateway is running on port ${PORT}`);
+  logger.info(`Redis url is running on port ${process.env.REDIS_URL}`);
+  logger.info(
+    `Identity service is running on port ${process.env.IDENTITY_SERVICE_URL}`
+  );
+});
